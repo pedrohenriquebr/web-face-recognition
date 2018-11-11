4e3dc9f9ebd463056940a971976a46768801a41f
PedroHenriqueBraga <pedrohenriquebraga735@gmail.com>
2018-11-11 19:50:52 -0200

    modelo cnn

--- src/app.py
+++ src/app.py
@@ -15,23 +15,27 @@ def allowed_file(filename):
 	return '.' in filename and \
 		   filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/api/recognition',methods=['POST','GET'])
+@app.route('/api/recognition',methods=['POST'])
 def face_recognition_api():
-	# Check if a valid image file was uploaded
+	# por enquanto somente KNN
 	if request.method == 'POST':
 		if 'file' not in request.files:
 			return redirect(request.url)
-
+		
 		file = request.files['file']
+		model = request.values['model']  # por padrão será o Hog
 
+		if model == None:
+			model= 'hog'
+		
 		if file.filename == '':
 			return redirect(request.url)
 
 		if file and allowed_file(file.filename):
 			# The image file seems valid! Detect faces and return the result.
-			return detect_faces_in_image(file)
-	elif request.method == 'GET':
-		
+			return detect_faces_in_image(file,model=model)
+	
+
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_image():
@@ -52,7 +56,7 @@ def upload_image():
 	# If no valid image file was uploaded, show the file upload form:
 	return '''
 	<!doctype html>
-	<title>Reconhecimento Facial</title>
+	<title>Reconhecimento Facial com KNN!!</title>
 	<h1>Envie uma imagem para o reconhecimento facial !</h1>
 	<form method="POST" enctype="multipart/form-data">
 	  <input type="file" name="file">
@@ -61,19 +65,33 @@ def upload_image():
 	'''
 
 
-def detect_faces_in_image(file_stream):
+def detect_faces_in_image(file_stream,model='hog'):
 	img  = face_recognition.load_image_file(file_stream)
-	knn_model = os.path.join(os.getenv('MODELSET_DIR'), os.getenv('KNN_MODEL'))
-	result = prediction.predict_frame(img,model_path=knn_model)
+	if model == 'cnn':
+		classifier_model = os.path.join(os.getenv('MODELSET_DIR'), os.getenv('CNN_MODEL'))
+	else:
+		classifier_model = os.path.join(os.getenv('MODELSET_DIR'), os.getenv('KNN_MODEL'))
+		
+	result = prediction.predict_frame(img,model_path=classifier_model,model=model)
 	return jsonify(result)		   
 
-@app.route("/train/knn")
+@app.route("/train")
 def train_dataset():
 	train_folder = os.getenv('DATASET_DIR')
-	knn_model = os.path.join(os.getenv('MODELSET_DIR'), os.getenv('KNN_MODEL'))
-	classifier = training.train(train_folder, model_save_path=knn_model, n_neighbors=1,verbose=True)
+	model = request.values['model']
+
+	if model == None:
+		model = 'hog'
+
+	if model == 'cnn':
+		cnn_model = os.path.join(os.getenv('MODELSET_DIR'), os.getenv('CNN_MODEL'))
+		classifier = training.train(train_folder, model_save_path=cnn_model, n_neighbors=1,verbose=True,model='cnn')
+	else:
+		knn_model = os.path.join(os.getenv('MODELSET_DIR'), os.getenv('KNN_MODEL'))
+		classifier = training.train(train_folder, model_save_path=knn_model, n_neighbors=1,verbose=True)
+
 	return 'Treinamento completo !!'
 
- 
+	
 if __name__ == "__main__":
 	app.run(debug=True,port=5000,host='0.0.0.0')
