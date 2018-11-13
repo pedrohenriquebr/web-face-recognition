28bd65c66d5ac2774eec09f3af7a5631393f1c34
PedroHenriqueBraga <pedrohenriquebraga735@gmail.com>
2018-11-13 00:37:00 -0200

    flask debug

--- src/app.py
+++ src/app.py
@@ -1,12 +1,11 @@
-from flask import Flask, jsonify, request, redirect, render_template
-from werkzeug.utils import secure_filename
-import training 
-import prediction
-import face_recognition
 import os
+
+import face_recognition
+import prediction
 import settings
-
-
+import training
+from flask import Flask, jsonify, redirect, render_template, request
+from werkzeug.utils import secure_filename
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
@@ -53,16 +52,24 @@ def upload_image():
 
 		if file and allowed_file(file.filename):
 			# The image file seems valid! Detect faces and return the result.
-			return detect_faces_in_image(file,model=os.getenv('FACE_DETECTION_MODEL','hog'))
-
+			FACE_DETECTION_MODEL = os.getenv('FACE_DETECTION_MODEL','hog')
+			app.logger.debug('FACE_DETECTION_MODEL: {}'.format(FACE_DETECTION_MODEL))
+			return detect_faces_in_image(file,model=FACE_DETECTION_MODEL)
+	
 	return render_template('index.html')
 
 
 def detect_faces_in_image(file_stream,model='hog'):
+	MODELSET_DIR = os.getenv('MODELSET_DIR')
+	KNN_MODEL  = os.getenv('KNN_MODEL')
+
+	app.logger.debug('MODELSET_DIR: {}'.format(MODELSET_DIR))
+	app.logger.debug('KNN_MODEL: {}'.format(KNN_MODEL))
+
 	img  = face_recognition.load_image_file(file_stream)
-	classifier_model = os.path.join(os.getenv('MODELSET_DIR'), os.getenv('KNN_MODEL'))
+	classifier_model = os.path.join(MODELSET_DIR, KNN_MODEL)
 		
-	result = prediction.predict_frame(img,model_path=classifier_model,model='cnn')
+	result = prediction.predict_frame(img,model_path=classifier_model,model=model)
 	return jsonify(result)		   
 
 
