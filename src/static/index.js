b024c637bb8e8a55a44a6f9836a4790794d763d0
PedroHenriqueBraga <pedrohenriquebraga735@gmail.com>
2018-11-18 16:12:23 -0200

    adicionado index.js

--- src/static/index.js
+++ src/static/index.js
@@ -1,17 +1,78 @@
+var canvas = null;
+var ctx = null;
+var JSON_PRETTY = function (string) {
+
+    var pretty = {
+      "parse": function (member) {
+        return this[(member == undefined) ? 'null' : member.constructor.name.toLowerCase()](member);
+      },
+  
+      "null": function (value) {
+        return this['value']('null', 'null');
+      },
+      "array": function (value) {
+        var results = '';
+        for (var x=0; x < value.length; x++) {
+          results += '<li>' + this['parse'](value[x]) + '</li>';
+        }
+        return '[ ' + ((results.length > 0) ? '<ul class="array">' + results + '</ul>' : '') + ' ]';
+      },
+      "object": function (value) {
+        var results = '';
+        for (member in value) {
+          results += '<li>' + this['value']('object', member) + ': ' + this['parse'](value[member]) + '</li>';
+        }
+        return '{ ' + ((results.length > 0) ? '<ul class="object">' + results + '</ul>' : '') + ' }';
+      },
+      "number": function (value) {
+        return this['value']('number', value);
+      },
+      "string": function (value) {
+        return this['value']('string', value);
+      },
+      "boolean": function (value) {
+        return this['value']('boolean', value);
+      },
+  
+      "value": function (type, value) {
+        if (/^(http|https):\/\/[^\s]+$/.test(value)) {
+          return this['value'](type, '<a href="' + value + '" target="_blank">' + value + '</a>');
+        }
+        return '<span class="' + type + '">' + value + '</span>';
+      }
+    };
+  
+    var parse = {
+      "error": function (error) {
+        return '<h1>Unable to parse JSON.</h1><p><h2>Error Message:</h2><textarea>' + error + '</textarea><br /><br /><h2>Response:</h2><textarea>' + string + '</textarea></p>';
+      }
+    }
+  
+    try {
+      var output = pretty.parse (eval ('(' + string + ')'));
+    }
+    catch (error) {
+      var output = parse.error (error);
+    }
+  
+    return output;
+  };
+
 $(function () {
     //http://jsfiddle.net/influenztial/qy7h5/
     //https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Using_images
     //https://www.w3schools.com/tags/canvas_rect.asp
     //https://www.w3schools.com/tags/canvas_drawimage.asp
     var imageLoader = $('#imageLoader')[0];
+    canvas = document.getElementById('imageCanvas');
+    ctx = canvas.getContext('2d');
     imageLoader.addEventListener('change', handleImage, false);
 });
 
 function handleImage(e) {
-    var canvas = document.getElementById('imageCanvas');
-    var ctx = canvas.getContext('2d');
     var reader = new FileReader();
-    clear_fields();
+    clear_all();
+    sendImage();
     reader.onload = function (event) {
         var img = new Image();
         img.onload = function () {
@@ -24,13 +85,13 @@ function handleImage(e) {
         img.src = event.target.result;
     }
     reader.readAsDataURL(e.target.files[0]);
-    sendImage();
+
 }
 
 
-function clear_fields() {
+function clear_all() {
     $("#resultado").html("");
-
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 function sendImage() {
@@ -48,7 +109,7 @@ function sendImage() {
             var array = result;
             var json = JSON.stringify(result);
             console.log('Typeof: ' + typeof (array));
-            $("#resultado").html(json);
+            $("#resultado").html(JSON_PRETTY(json));
             drawFaceLocation(array);
         },
         error: function (xhr, resp, text) {
@@ -58,9 +119,6 @@ function sendImage() {
 }
 
 function drawFaceLocation(array) {
-    var canvas = document.getElementById('imageCanvas');
-    var ctx = canvas.getContext('2d');
-
     for (n in array) {
         var persons = array[n];
         var name = persons[0];
@@ -87,9 +145,5 @@ function drawFaceLocation(array) {
         ctx.textAlign = 'center';
         ctx.fillText(name, left_ + (ctx.measureText(name).width / 2), (bottom_ + size));
         ctx.stroke();
-
-        var dataURL = canvas.toDataURL();
-        document.getElementById('canvasImg').src = dataURL;
-
-}
+    }
 }
