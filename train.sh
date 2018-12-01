0b73cc473328324b793389fa155aa3ca79605586
PedroHenriqueBraga <pedrohenriquebraga735@gmail.com>
2018-12-01 18:56:05 -0200

    atualiza train.sh

--- train.sh
+++ train.sh
@@ -7,9 +7,7 @@ function docker_compose_check(){
     if  ! command -v docker-compose  > /dev/null;
     then
         echo "instalando docker-compose..."
-
-        sudo curl -L https://github.com/docker/compose/releases/download/1.23.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-        sudo chmod +x /usr/local/bin/docker-compose
+        sudo pip3 install -U docker-compose
     fi
 }
 
