4c86cb2861d3a38836356d3c001756acbda1a941
PedroHenriqueBraga <pedrohenriquebraga735@gmail.com>
2019-02-06 17:15:07 -0200

    Merge the Dockerfiles #12

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,6 @@ function build_image(){
 
 function building(){
     # Base images building
-    echo "Building the image base_face_recognition:latest"
-    build_image "base_face_recognition/Dockerfile" "base_face_recognition:latest" "base_face_recognition"
-
     echo "Building the image $DOCKER_USERNAME/web_face_recognition:$TAG"
     build_image "$PWD/Dockerfile" "web_face_recognition:latest" "$PWD"
     docker tag "web_face_recognition:latest" "$DOCKER_USERNAME/web_face_recognition:$TAG"
