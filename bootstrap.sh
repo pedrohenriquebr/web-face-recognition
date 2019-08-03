d5721ebf773d8392555d450243215c6741ca6712
pedrohenriquebr <pedrohenriquebraga735@gmail.com>
2019-08-03 10:48:00 -0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,20 +3,20 @@
 test -n "$DEBUG" && set -x
 
 function docker_compose_check(){
-    echo "verificando docker-compose..."
+    echo "Verifying docker-compose..."
     if  ! command -v docker-compose  > /dev/null;
     then
-        echo "instalando docker-compose..."
+        echo "installing docker-compose..."
         sudo pip3 install -U docker-compose
     fi
 }
 
 
 function docker_check(){
-    echo "verificando docker..."
+    echo "Verifying docker..."
     if !  command -v docker > /dev/null ; 
     then
-        echo "instalando docker..."
+        echo "installing docker..."
         curl -fsSL https://get.docker.com -o /tmp/get-docker.sh
         sh /tmp/get-docker.sh
         sudo usermod -aG docker $USER 
@@ -26,4 +26,45 @@ function docker_check(){
 # Instalo algumas dependências
 docker_check
 docker_compose_check
-mkdir -p {modelset,dataset,testset}
+
+op=""
+while   [ "$op"  != 'y' ] && [ "$op" !=  'n' ] && [ "$op" != 'Y'  ] && [ "$op" != 'N' ] ; do 
+    read -p 'Do you want install dlib?' op
+done
+
+if [ "$op" = 'N' ] || [ "$op" = 'n' ]; then
+    exit 1
+fi
+
+sudo su << EOF
+apt-get -y update
+apt-get install -y --fix-missing \
+    build-essential \
+    cmake \
+    gfortran \
+    git \
+    wget \
+    curl \
+    graphicsmagick \
+    libgraphicsmagick1-dev \
+    libatlas-dev \
+    libavcodec-dev \
+    libavformat-dev \
+    libgtk2.0-dev \
+    libjpeg-dev \
+    liblapack-dev \
+    libswscale-dev \
+    pkg-config \
+    python3-dev \
+    python3-numpy \
+    python3-pip \
+    software-properties-common \
+    zip \
+    && apt-get clean && rm -rf /tmp/* /var/tmp/*
+
+cd ~ && mkdir -p dlib && \
+    git clone -b 'master' --single-branch https://github.com/davisking/dlib.git dlib/ && \
+    cd  dlib/ && \
+    python3 setup.py install
+
+EOF
